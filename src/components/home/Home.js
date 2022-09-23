3db9fa76f449591dcb3c953b9ed9b37031722afb
OkoroaforKelechiDivine <106816493+OkoroaforKelechiDivine@users.noreply.github.com>
2022-09-23 02:23:42 +0100

    fixed navbar

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,10 +1,30 @@
 import React from "react";
 import "./home.css"
+import logo from "../images/company_logo.png"
 
 function Home(){
 
     return(
-        <div>Hello</div>
+        <div className={"page-body"}>
+            <header>
+                <a href={""}>
+                    <img src={logo} alt={""} className={"logo"}/>
+                </a>
+                <nav>
+                    <ul className={"nav_links"}>
+                        <li><a href={"#"}>Home</a> </li>
+                        <li><a href={"#"}>About</a> </li>
+                        <li><a href={"#"}>Brokage</a> </li>
+                        <li><a href={"#"}>Management</a> </li>
+                        <li><a href={"#"}>Investment</a> </li>
+                        <li><a href={"#"}>Development</a> </li>
+                        <li><a href={"#"}>Blog</a> </li>
+                        <li><a href={"#"}>Contact</a> </li>
+
+                    </ul>
+                </nav>
+            </header>
+        </div>
 
     )
 }
