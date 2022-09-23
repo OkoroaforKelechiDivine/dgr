836c6c28df2618a9b6548abd73cc8b43f71f8e84
OkoroaforKelechiDivine <106816493+OkoroaforKelechiDivine@users.noreply.github.com>
2022-09-23 11:42:28 +0100

    fixing second section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Home from "./components/home/Home";
+import Contact from "./components/contact/Contact";
 
 function App() {
   return (
     <div className={""}>
-      <Home/>
+      {/*<Home/>*/}
+        <Contact />
     </div>
   );
 }
